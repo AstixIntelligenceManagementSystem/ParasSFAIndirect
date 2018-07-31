220b51091457b6fc377ea6ec761007011f42f0ab
Nitish Dubey <astixset2@gmail.com>
2018-07-31 13:51:36 +0530

    IInitial Commit

---
+++ app/src/main/java/project/astix/com/parassfaindirect/CategoryCommunicatorCityState.java
@@ -0,0 +1,10 @@
+package project.astix.com.parassfaindirect;
+
+import android.app.Dialog;
+
+public interface CategoryCommunicatorCityState {
+	
+	public void selectedCityState(String selectedCategory, Dialog dialog, int flgCityState);
+
+
+}
---
+++ app/src/main/java/project/astix/com/parassfaindirect/TaskListner.java
@@ -0,0 +1,6 @@
+package project.astix.com.parassfaindirect;
+
+public interface TaskListner
+{
+    void onTaskFinish(boolean b, int returnFrom);
+}
